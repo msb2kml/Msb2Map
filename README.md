c5bad85bc9e955f718434003e4dcb8fb3a949089
msb2kml <34025111+msb2kml@users.noreply.github.com>
2019-04-23 09:52:00 +0200

    Initial Release

---
+++ README.md
@@ -0,0 +1,75 @@
+# Purpose
+The Msb2Map Android application is designed to display on a map
+the running position of a RC plane as computed by the
+[Msb2And](https://github.com/msb2kml/Msb2And) application from
+the Multiplex RC telemetry data.  
+The minimum version of Msb2And is 1.2.
+
+# Installation
+The application should be compiled from the sources in this repository
+or downloaded from the "**Releases**" pages.
+
+You need to have authorized the installation of applications from other sources than Google Play.
+
+# Acknowledgments
+This application make use of the library
+[osmdroid](https://github.com/osmdroid/osmdroid).
+
+The map is provided by the [OpenStreetMap](https://www.openstreetmap.org/)
+Foundation.
+
+# Standalone
+This application is designed to be used in conjunction with the Msb2And
+application; however it could be launched directly.  
+It shows then all the locations stored in the file MSBlog/StartGPS.gpx :
+see the explanations for [Msb2Kml](https://github.com/msb2kml/Msb2Kml/blob/RemoteGPS/Documents/RemoteGPS.md) and for
+[Msb2And](https://github.com/msb2kml/Msb2And/blob/master/Documents/RemoteGPS.md).  
+The locations are displayed as markers with a form of diabolo.  
+Taping on a marker shows a bubble with the name of the location.  
+Taping on a bubble hide it.
+
+Locations as turn points could be defined in the file StrartGPS.gpx
+with the help of the [Msb2Kml](https://github.com/msb2kml/Msb2Kml/)
+application.
+
+A button in the right lower corner let you close this application
+and maybe return to the Msb2And application. 
+An information field on the side of this button show the number
+of locations found in the StartGPS.gpx file.
+
+The zoom level and the orientation of the map are adjustable with
+a two fingers gesture. 
+The map may be moved by dragging with a finger.
+
+# Launched by Msb2And
+When launched through Msb2And, a continuous flow of data is received.  
+The current location of the plane is noted as a marker with a form
+of reticule and the map is kept centered on this marker.  
+The information field show the value of the parameter that has
+been associated by Msb2And.
+
+# Getting the map
+The tiles composing the map are downloaded from the server of the
+"**Open Street Map Foundation**" and are kept in a local cache while they
+are valid. 
+Access to the Internet is thus needed at some time.  
+But if one has had a look at the region of interest with the appropriate
+zoom level, all the needed tiles are in the cache and no further
+Internet access is needed.
+
+The cache is common to most applications using the "**osmdroid**" library
+like the demo application available from [F-Droid](https://f-droid.org/packages/org.osmdroid/)
+or from [Google Play](https://play.google.com/store/apps/details?id=org.osmdroid).
+
+# Permissions and Android version
+This application should work on Android versions from
+Jelly Bean (4.1) to Oreo (8.0).  
+It need access to the Internet and to the storage.  
+Access to the location service (GPS) is not needed.
+
+# Screenshot
+
+![Msb2Map.jpg](Msb2Map.jpg)
+
+
+
---
+++ app/src/main/java/org/js/msb2map/MainActivity.java
@@ -0,0 +1,213 @@
+package org.js.msb2map;
+
+import android.Manifest;
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.CopyrightOverlay;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
+
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class MainActivity extends AppCompatActivity {
+
+    MapView map=null;
+    Context ctx;
+    String exPath= Environment.getExternalStorageDirectory().getAbsolutePath();
+    Marker startMarker;
+    Double latitude=48.8583;
+    Double longitude=2.2944;
+    Double zoom=15.0;
+    Location center=null;
+    String pathStartGPS=exPath+"/MSBlog/StartGPS.gpx";
+    Marker flyMarker=null;
+    IMapController mapController;
+    IntentFilter filter=new IntentFilter("org.js.LOC");
+    Boolean listening=false;
+    Button bMsb2And;
+    TextView vInfo;
+    String caller=null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        setContentView(R.layout.activity_main);
+        Intent intent=getIntent();
+        caller=intent.getStringExtra("CALLER");
+        center=(Location) intent.getParcelableExtra("CENTER");
+        if (center!=null){
+            latitude=center.getLatitude();
+            longitude=center.getLongitude();
+        }
+        zoom=intent.getDoubleExtra("ZOOM",zoom);
+        checkStorage();
+    }
+
+    void checkStorage(){
+        String state=Environment.getExternalStorageState();
+        Boolean montedSD=state.contains(Environment.MEDIA_MOUNTED);
+        if (!montedSD){
+            Toast.makeText(ctx,exPath+" not mounted!",Toast.LENGTH_LONG).show();
+            finish();
+        }
+        Boolean writeSD=!Environment.MEDIA_MOUNTED_READ_ONLY.equals(state);
+        if (!writeSD){
+            Toast.makeText(ctx,exPath+" not writable.",Toast.LENGTH_LONG).show();
+        }
+        Boolean hasPermission=(ContextCompat.checkSelfPermission(ctx,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED);
+        if (!hasPermission){
+            Toast.makeText(ctx,"This application need tp read "+exPath+".",Toast.LENGTH_LONG).show();
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        } else strtMap();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int reqCode, String[] permissions,int[] grantResults){
+        super.onRequestPermissionsResult(reqCode,permissions,grantResults);
+        if (reqCode==1){
+            if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                strtMap();
+            } else {
+                Toast.makeText(ctx,"Not granted!",Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+    public void onResume(){
+        super.onResume();
+        if (listening) registerReceiver(mReceiver,filter);
+    }
+
+    public void onPause(){
+        super.onPause();
+        if (listening) unregisterReceiver(mReceiver);
+    }
+
+    public void launchMsb2And(){
+        finish();
+    }
+
+    public void strtMap(){
+        StartGPS sg;
+        Map<String,Location> startPt;
+        map=(MapView) findViewById(R.id.map);
+        bMsb2And=(Button) findViewById(R.id.msb2and);
+        vInfo=(TextView) findViewById(R.id.vInfo);
+        if (caller==null){
+            bMsb2And.setText("Exit");
+        } else {
+            bMsb2And.setText(caller);
+        }
+        bMsb2And.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchMsb2And();
+            }
+        });
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
+        RotationGestureOverlay mRotationGestureOverlay = new RotationGestureOverlay(map);
+        mRotationGestureOverlay.setEnabled(true);
+        map.setMultiTouchControls(true);
+        map.getOverlays().add(mRotationGestureOverlay);
+        map.getOverlays().add(new CopyrightOverlay(ctx));
+        mapController = map.getController();
+        sg=new StartGPS(pathStartGPS);
+        startPt=sg.readSG();
+        Integer nPt=0;
+        if (!startPt.isEmpty()){
+            SortedSet<String> keys=new TreeSet<>();
+            keys.addAll(startPt.keySet());
+            for (String here: keys){
+                if (center==null){
+                    center=startPt.get(here);
+                    latitude=center.getLatitude();
+                    longitude=center.getLongitude();
+                }
+                Double lat=startPt.get(here).getLatitude();
+                Double lon=startPt.get(here).getLongitude();
+                Marker m=new Marker(map);
+                m.setIcon(getResources().getDrawable(R.drawable.diabolo));
+                m.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_CENTER);
+                m.setPosition(new GeoPoint(lat,lon));
+                m.setTitle(here);
+                map.getOverlays().add(m);
+                nPt++;
+            }
+        }
+        mapController.setZoom(zoom);
+        mapController.setCenter(new GeoPoint(latitude,longitude));
+        ScaleBarOverlay scale=new ScaleBarOverlay(map);
+        scale.setUnitsOfMeasure(ScaleBarOverlay.UnitsOfMeasure.metric);
+        map.getOverlays().add(scale);
+        map.invalidate();
+        vInfo.setText("Nb. Location: "+nPt);
+        registerReceiver(mReceiver,filter);
+        listening=true;
+    }
+
+    public void update(Location loc, String bubble){
+        GeoPoint gp=new GeoPoint(loc.getLatitude(),loc.getLongitude());
+        if (flyMarker!=null) flyMarker.remove(map);
+        flyMarker=new Marker(map);
+        flyMarker.setIcon(getResources().getDrawable(R.drawable.target));
+        flyMarker.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_CENTER);
+        flyMarker.setPosition(gp);
+        if (flyMarker==null){
+            flyMarker=new Marker(map);
+            flyMarker.setIcon(getResources().getDrawable(R.drawable.target));
+            flyMarker.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_CENTER);
+            flyMarker.setPosition(gp);
+            map.getOverlays().add(flyMarker);
+        }
+        flyMarker.setPosition(gp);
+        map.getOverlays().add(flyMarker);
+        mapController.setCenter(gp);
+        map.invalidate();
+        if (bubble!=null) {
+            vInfo.setText(bubble);
+        }
+    }
+
+    private final BroadcastReceiver mReceiver=new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Location loc=(Location) intent.getParcelableExtra("LOC");
+            String bubble=intent.getStringExtra("BUBBLE");
+            if (loc!=null) update(loc,bubble);
+        }
+    };
+}
